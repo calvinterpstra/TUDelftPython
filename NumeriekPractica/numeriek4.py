fd45994214337592ec739db8791dc8f11e809e09
calvinterpstra <calvinterpstra@gmail.com>
2019-06-05 10:56:15 +0200

    Update numeriek4.py

--- NumeriekPractica/numeriek4.py
+++ NumeriekPractica/numeriek4.py
@@ -4,35 +4,52 @@ from math import *
 import numpy as np
 import matplotlib.pyplot as plt
 
-dt = 0.000390625
-t_f = np.linspace
 u = 0
 v = 0
-w_n = np.array([u, v])
 a = 2
 b = 4.5
+p = 4
 
-def f(t_n, w_n):
-    du = a(t_n) - b*w_n[0] + w_n[1]*w_n[0]**2 - w_n[0]
+def f(t_n, w_n, t_f):
+    du = a(t_n, t_f) - b*w_n[0] + w_n[1]*w_n[0]**2 - w_n[0]
     dv = b*w_n[0] - w_n[1]*w_n[0]**2
     return np.array([du, dv])
 
-def a(t):
+def a(t, t_f):
     if(t > t_f):
         return 2*np.e**(t_f - t)
     else:
         return 2
 
-for(tf_i in t_f):
+def truncErr(w_n, w_n_2):
+    return (w_n - w_n_2)/15.0
+
+def rk4(dt, t_end, t_f):
+    w_n = np.array([[u, v]])
     t = 0
-while(t <= 2):
-    k1 = dt * f(t, w_n)
-    k2 = dt * f(t + 0.5*dt, w_n + 0.5*k1)
-    k3 = dt * f(t + 0.5*dt, w_n + 0.5*k2)
-    k4 = dt * f(t + dt, w_n + k3)
+    while(t < t_end):
+        k1 = dt * f(t, w_n[-1], t_f)
+        k2 = dt * f(t + 0.5*dt, w_n[-1] + 0.5*k1, t_f)
+        k3 = dt * f(t + 0.5*dt, w_n[-1] + 0.5*k2, t_f)
+        k4 = dt * f(t + dt, w_n[-1] + k3, t_f)
 
-    w_n1 = w_n + (1/6)*(k1 + 2*k2 + 2*k3 + k4)
-    print("t: ", t, "u: ", w_n[0], "v: ", w_n[1])
+        w_n_new = w_n[-1] + (1/6)*(k1 + 2*k2 + 2*k3 + k4)
+        w_n = np.append(w_n, np.array([w_n_new]), axis=0)
+        t = round(t+dt, 10)
 
-    w_n = w_n1
-    t += dt
+    return w_n
+
+k = 5
+dt = 0.2/2**k
+t_f = np.linspace(0, 10, 100)
+v_new = np.linspace(0, 0, 100)
+
+for i in range(len(t_f)):
+    v_new[i] = rk4(dt, 10, t_f[i])[-1][1]
+
+plt.plot(t_f, v_new)
+plt.show()
+
+v_max_pos = np.argmax(v_new)
+t_f_max = t_f[v_max_pos]
+print("t_f_max: ", t_f_max)
