9e38c0285d2b9f48275941116b51b01b705d83a6
calvinterpstra <calvinterpstra@gmail.com>
2019-10-28 18:06:20 +0100

    Added clarification with comments and formatted outputs

--- Minor/robot_mechanics.py
+++ Minor/robot_mechanics.py
@@ -2,48 +2,35 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# r1 = 191
-# m1 = 4100
-# r2 = 191
-# m2 = 100
-# r3 = 191
-# m3 = 3900
-# r4 = 19.1
-# m4 = 10200
-# r5 = 19.1
-# m5 = 10000
-
+# RE 50 24V
 nominal_torque = 405 
 no_load_r = 5950 
 r_m_gradient = 0.668 
 operating_range_r = [0, 6000, 12000]
-operating_range_m = [nominal_torque, nominal_torque*0.95, nominal_torque*0.87]
+operating_range_m = [nominal_torque, nominal_torque*0.93, nominal_torque*0.80]
 electrical_limit_r = [no_load_r, (no_load_r - nominal_torque*r_m_gradient)]
 electrical_limit_m = [0, nominal_torque]
 
-gear_limit_m = 10200/(nominal_torque*0.98)
-gear_limit_r = (no_load_r - (nominal_torque*(4100/10200))*r_m_gradient)/191
+# Calculate gearing limits
+gear_limit_m = 10200/(nominal_torque*0.985)
+gear_limit_r = (no_load_r - (nominal_torque*(4100/10200))*r_m_gradient)/(20*30/np.pi) #/191
 for i in range(100):
     gear_limit_r = (no_load_r - (4100/gear_limit_r)*r_m_gradient)/191
 
-gearing = gear_limit_m
-r1 = 191 * gearing
+gearing = gear_limit_r # using the electrical limit threshold as the gearing limit
+# Calculate torques and rotational velocities for each scenario
+r1 = (20*30/np.pi) * gearing
 m1 = 4100 / gearing
-r2 = 191 * gearing
+r2 = (20*30/np.pi) * gearing
 m2 = 100 / gearing
-r3 = 191 * gearing
+r3 = (20*30/np.pi) * gearing
 m3 = 3900 / gearing
-r4 = 19.1 * gearing
+r4 = (2*30/np.pi) * gearing
 m4 = 10200 / gearing
-r5 = 19.1 * gearing
+r5 = (2*30/np.pi) * gearing
 m5 = 10000 / gearing
 
-
-# plt.plot(r1, m1, 'o', label='1')
-# plt.plot(r2, m2, 'o', label='2')
-# plt.plot(r3, m3, 'o', label='3')
-# plt.plot(r4, m4, 'o', label='4')
-# plt.plot(r5, m5, 'o', label='5')
+# Plotting
 plt.plot(m1, r1, 'o', label='1')
 plt.plot(m2, r2, 'o', label='2')
 plt.plot(m3, r3, 'o', label='3')
@@ -55,11 +42,23 @@ plt.plot(operating_range_m, operating_range_r, 'r--', label='Continuous operatin
 plt.xlabel('M (mNm)')
 plt.ylabel('$\omega$ (rpm)')
 
-plt.title("Torque vs rotational velocity with gearing 100:1")
+plt.title("Torque vs rotational velocity with gearing {:.1f}:1".format(gearing))
 
 plt.legend()
 
-print("r limit:", gear_limit_r)
-print("m limit:", gear_limit_m)
+# Print gearing limits
+print("r limit: {:.1f}:1".format(gear_limit_r))
+print("m limit: {:.1f}:1".format(gear_limit_m))
 
 plt.show()
+
+# Calculations for Q6
+I = 5.36*10**(-5)
+M_m = I*(20*gearing)/0.5
+M_r = 4.1/gearing
+
+print("omega: {:.3f} rad/s".format(20*gearing))
+print("alpha: {:.3f} rad/s^2".format(40*gearing))
+print("M_m: {:.1f} mNm".format(M_m*1000))
+print("M_r: {:.1f} mNm".format(M_r*1000))
+print("ratio: {:.3f}".format(M_m/M_r))
